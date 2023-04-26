9824dcac62e219172f3a2e680a984cd1e186d58c
nadia.artamonova@gmail.com <nadia.artamonova@gmail.com>
2023-04-26 12:55:41 +0300

    task 2

---
+++ taks2/script.js
@@ -0,0 +1,16 @@
+{
+    const productName = 'Apple';
+    const quentity = 12;
+    const productCategory = 'Fruits';
+    const productPrice = 5;
+
+    console.log("Product: ", productName);
+}
+
+{
+    const productName = 'Tomato';
+    const quentity = 3;
+    const productCategory = 'Fruits';
+    const productPrice = 9;
+    console.log(`Category: ${productCategory} \nProduct: ${productName}`);
+}
