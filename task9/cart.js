69af538b169bd75bbd16befcc8fd13a267871232
nadia.artamonova@gmail.com <nadia.artamonova@gmail.com>
2023-04-29 11:25:46 +0300

    fix 9

--- task9/cart.js
+++ task9/cart.js
@@ -6,33 +6,33 @@
         items: [],
         totalPrice: 0,
         count: 0,
-        getTotalPrice(cart){
-            return cart.totalPrice;
+        getTotalPrice(){
+            return this.totalPrice;
         },
         add(name, price, count = 1){
 
-            cart.items.push([name, price, count]);
-            cart.totalPrice = cart.calculateItemPrice();
-            cart.count = cart.increaseCount(count);
+            this.items.push([name, price, count]);
+            cart.totalPrice = this.calculateItemPrice();
+            cart.count = this.increaseCount(count);
         },
         increaseCount(count){
-            return cart.count + count;
+            return this.count + count;
         },
         calculateItemPrice(){
-            cart.totalPrice = cart.items.reduce((total, item) => {
+            this.totalPrice = this.items.reduce((total, item) => {
                 return total + (item[1] * item[2]);
             }, 0);
-            return cart.totalPrice;
+            return this.totalPrice;
         }, 
-        clear(cart){
-            cart.items = [];
-            totalPrice = 0;
-            count = 0;
-            return cart;
+        clear(){
+            this.items = [];
+            this.totalPrice = 0;
+            this.count = 0;
+            return this;
         },
-        print(cart){
-            console.log(JSON.stringify(cart.items));
-            console.log(`Total price: ${cart.totalPrice}`);
+        print(){
+            console.log(JSON.stringify(this.items));
+            console.log(`Total price: ${this.totalPrice}`);
         },
     }
 
@@ -40,7 +40,7 @@
     cart.add('milk', 1, 3);
     cart.add('juice', 3, 6);
 
-    cart.print(cart);
+    cart.print();
 
 
 }
